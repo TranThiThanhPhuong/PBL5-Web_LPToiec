9e2a91614661a7ab5351bfcb9a19506edf8aa7da
TranThiThanhPhuong <phuongsuga333@gmail.com>
2025-03-04 02:42:39 +0700

    4/3

--- src/main/java/com/example/demo/controller/user/UserController.java
+++ src/main/java/com/example/demo/controller/user/UserController.java
@@ -16,4 +16,59 @@ public class UserController {
     public String userProfile() {
         return "user/profile-user";
     }
+    
+    @RequestMapping("/vocal")
+    public String userVocalbulary() {
+        return "user/vocalbulary";
+    }
+    
+    @RequestMapping("/gramm")
+    public String userGrammar() {
+        return "user/grammar";
+    }
+    
+    @RequestMapping("/listen")
+    public String userListening() {
+        return "user/listeningcard";
+    }
+    
+    @RequestMapping("/listenp1")
+    public String userListeningPart1() {
+        return "user/listeningpart1";
+    }
+    
+    @RequestMapping("/listenp2")
+    public String userListeningPart2() {
+        return "user/listeningpart2";
+    }
+    
+    @RequestMapping("/listenp3")
+    public String userListeningPart3() {
+        return "user/listeningpart3";
+    }
+    
+    @RequestMapping("/listenp4")
+    public String userListeningPart4() {
+        return "user/listeningpart4";
+    }
+    
+    @RequestMapping("/reading")
+    public String userReading() {
+        return "user/readingcard";
+    }
+    
+    @RequestMapping("/readingp5")
+    public String userReadingPart5() {
+        return "user/readingpart5";
+    }
+    
+    @RequestMapping("/readingp6")
+    public String userReadingPart6() {
+        return "user/readingpart6";
+    }
+    
+    @RequestMapping("/readingp7")
+    public String userReadingPart7() {
+        return "user/readingpart7";
+    }
 }
